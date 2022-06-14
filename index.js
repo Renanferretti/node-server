3221340eeb530f70564b51ba23dc23ee5c6133b5
Renan Ferretti <40603756+Renanferretti@users.noreply.github.com>
2022-06-14 15:42:39 -0300

    atualizando para o meu mongo DB
    estava com a uri errada, pois era a do exemplo do professor (que foi usada no computador dele). Logo, atualizei para a minha

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function notify(noteId, title, description){
 const mongodb = require('mongodb')
 const ObjectId = mongodb.ObjectId;
 const password = process.env.PASSWORD || "4Wi9saIkviR6Lhy1";
-const connectionString = `mongodb+srv://admin:${password}@cluster0.5qv4nd1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://admin:${password}@cluster0.sf4s11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const options = { 
     useNewUrlParser: true, 
